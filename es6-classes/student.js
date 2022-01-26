d473b3ecdfdf0ce75700a22ae16dace978693116
Sam Vivonia <snvivonia@gmail.com>
2022-01-25 21:19:23 -0800

    completed es6-classes

---
+++ es6-classes/student.js
@@ -0,0 +1,35 @@
+/* exported Student */
+
+class Student {
+  constructor(firstName, lastName, subject) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.subject = subject;
+  }
+  getFullName() {
+    const { firstName, lastName } = this;
+    return `${firstName} ${lastName}`;
+  }
+  getIntroduction() {
+    const { subject } = this;
+    const fullName = this.getFullName();
+    return `Hello, my name is ${fullName} and I am studying ${subject}.`;
+  }
+}
+
+// function Student(firstName, lastName, subject) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.subject = subject;
+// }
+
+// Student.prototype.getFullName = function () {
+//   const { firstName, lastName } = this;
+//   return `${firstName} ${lastName}`;
+// };
+
+// Student.prototype.getIntroduction = function () {
+//   const { subject } = this;
+//   const fullName = this.getFullName();
+//   return `Hello, my name is ${fullName} and I am studying ${subject}.`;
+// };
