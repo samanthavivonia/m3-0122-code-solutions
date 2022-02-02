2968e9f74b80e00c1eb5235db199b8bc4e670c4c
Sam Vivonia <snvivonia@gmail.com>
2022-02-01 19:29:00 -0800

    completed react-function-components

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import reactDom from "react-dom";
+import ReactDOM from "react-dom";
+
+const CustomButton = <button>Click Me!</button>;
+
+reactDom.render(CustomButton, document.querySelector('#root'));
