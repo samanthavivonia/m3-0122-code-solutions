96fcb3dcb0ba9ac7b3efdabf223372b19cf2b723
Sam Vivonia <snvivonia@gmail.com>
2022-02-01 20:01:09 -0800

    fixed react-function-components

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import reactDom from "react-dom";
 import ReactDOM from "react-dom";
 
-const CustomButton = <button>Click Me!</button>;
+function CustomButton() {
+  return <button>Click Me!</button>;
+};
 
 reactDom.render(CustomButton, document.querySelector('#root'));
