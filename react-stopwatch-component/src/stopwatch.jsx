c31cc8e172e651744bba2eda011399e7109b199d
Sam Vivonia <snvivonia@gmail.com>
2022-02-10 19:54:28 -0800

    Completed react stopwatch

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,13 +4,38 @@ import ReactDOM from "react-dom";
 class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { playing: false };
+    this.state = { playing: false , timerCount : 0};
     this.handleClick = this.handleClick.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
-  handleClick() {
-    console.log('clicked!')
+  handleClick () {
+    if (!this.state.playing) {
+      this.handleStart()
+    } else {
+      this.handleStop()
+    }
+  }
+
+  handleStart() {
+    console.log('Started!');
     this.setState({ playing: !this.state.playing });
+    this.timerID = window.setInterval(() => {
+      this.setState({ timerCount: this.state.timerCount+1});
+    }, 1000);
+  }
+
+  handleStop() {
+    console.log('Stopped!');
+    this.setState({ playing: !this.state.playing });
+    clearInterval(this.timerID);
+  }
+
+  handleReset() {
+    if (!this.state.playing) {
+      console.log('Reset!');
+      this.setState({ timerCount: 0})
+    }
   }
 
   render(props) {
@@ -22,10 +47,10 @@ class Stopwatch extends React.Component {
     }
     return (
       <div className="stopwatch">
-        <div className="stopwatch-seconds">
-          <h3>0</h3>
+        <div onClick={this.handleReset} className="stopwatch-seconds">
+          <h3>{this.state.timerCount}</h3>
         </div>
-        <div onClick={this.handleClick}className="stopwatch-button">
+        <div onClick={this.handleClick} className="stopwatch-button">
           <i className={`fas ${buttonIcon}`} />
         </div>
       </div>
