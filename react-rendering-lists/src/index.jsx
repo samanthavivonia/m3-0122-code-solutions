d950f0452e366cb1103246df2775bd6b8b3592cf
Sam Vivonia <snvivonia@gmail.com>
2022-02-14 20:46:48 -0800

    completed react rendering lists

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+function PokemonList(props) {
+  let pokemonNames = props.pokemonNames;
+  console.log('pokemonNames:', pokemonNames);
+  let listItems = pokemonNames.map((pokemon) => {
+    console.log('pokemon', pokemon)
+    return (
+      <li key={pokemon.number}>
+        {pokemon.name}
+      </li>
+    );
+  });
+  return <ul>{listItems}</ul>
+}
+
+ReactDOM.render(
+  <PokemonList pokemonNames={pokedex}/>,
+  document.getElementById('root')
+)
